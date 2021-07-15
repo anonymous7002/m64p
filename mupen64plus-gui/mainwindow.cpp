e7f9473a9afdd3d470c820a0e70fc34a1aaf78f5
anonymous7002 <joonatan.rinne@edu.poytya.fi>
2021-07-15 10:12:26 +0300

    test 1

--- mupen64plus-gui/mainwindow.cpp
+++ mupen64plus-gui/mainwindow.cpp
@@ -1105,10 +1105,6 @@ void MainWindow::loadPlugins()
     QString file_path;
     QString plugin_path;
 
-    file_path = "mupen64plus-video-parallel";
-    file_path += OSAL_DLL_EXTENSION;
-    plugin_path = QDir(pluginPath).filePath(file_path);
-
     res = osal_dynlib_open(&gfxPlugin, plugin_path.toUtf8().constData());
     if (res != M64ERR_SUCCESS)
     {
@@ -1119,10 +1115,6 @@ void MainWindow::loadPlugins()
 	
     PluginStartup = (ptr_PluginStartup) osal_dynlib_getproc(gfxPlugin, "PluginStartup");
     (*PluginStartup)(coreLib, (char*)"Video", DebugCallback);
-
-    file_path = "mupen64plus-audio-sdl2";
-    file_path += OSAL_DLL_EXTENSION;
-    plugin_path = QDir(pluginPath).filePath(file_path);
 
     res = osal_dynlib_open(&audioPlugin, plugin_path.toUtf8().constData());
     if (res != M64ERR_SUCCESS)
@@ -1146,12 +1138,7 @@ void MainWindow::loadPlugins()
         (*PluginStartup)(coreLib, this, DebugCallback);
     else
         (*PluginStartup)(coreLib, (char*)"Input", DebugCallback);
-
-
-    file_path = "mupen64plus-rsp-parallel";
-    file_path += OSAL_DLL_EXTENSION;
-    plugin_path = QDir(pluginPath).filePath(file_path);
-
+	
     res = osal_dynlib_open(&rspPlugin, plugin_path.toUtf8().constData());
     if (res != M64ERR_SUCCESS)
     {
--- mupen64plus-gui/settingsdialog.cpp
+++ mupen64plus-gui/settingsdialog.cpp
@@ -71,70 +71,49 @@ void SettingsDialog::handleConfigEdit()
 void SettingsDialog::initStuff()
 {
     layout = new QGridLayout(this);
-
-    QLabel *coreLabel = new QLabel("Core Library Path", this);
-    corePath = new QLineEdit(this);
-    corePath->setText(w->getSettings()->value("coreLibPath").toString());
-    QPushButton *coreButton = new QPushButton("Set Path", this);
-    connect(coreButton, SIGNAL (released()),this, SLOT (handleCoreButton()));
-    connect(corePath, SIGNAL (editingFinished()),this, SLOT (handleCoreEdit()));
-    corePath->setStyleSheet("border: 1px solid; padding: 10px");
-    layout->addWidget(coreLabel,0,0);
-    layout->addWidget(corePath,0,1);
-    layout->addWidget(coreButton,0,2);
-
-#ifdef CORE_LIBRARY_PATH
-    corePath->setEnabled(false);
-    coreButton->setEnabled(false);
-#endif
-
-    QLabel *pluginLabel = new QLabel("Plugin Dir Path", this);
-    pluginPath = new QLineEdit(this);
-    pluginPath->setText(w->getSettings()->value("pluginDirPath").toString());
-    QPushButton *pluginButton = new QPushButton("Set Path", this);
-    connect(pluginButton, SIGNAL (released()),this, SLOT (handlePluginButton()));
-    connect(pluginPath, SIGNAL (editingFinished()),this, SLOT (handlePluginEdit()));
-    pluginPath->setStyleSheet("border: 1px solid; padding: 10px");
-    layout->addWidget(pluginLabel,1,0);
-    layout->addWidget(pluginPath,1,1);
-    layout->addWidget(pluginButton,1,2);
-
-#ifdef PLUGIN_DIR_PATH
-    pluginPath->setEnabled(false);
-    pluginButton->setEnabled(false);
-#endif
-
-    QLabel *note = new QLabel("Note: If you change the Config Path, you need to close and re-open mupen64plus-gui before it will take effect.", this);
-    QLabel *configLabel = new QLabel("Config Dir Path", this);
-    configPath = new QLineEdit(this);
-    configPath->setText(w->getSettings()->value("configDirPath").toString());
-    QPushButton *configButton = new QPushButton("Set Path", this);
-    connect(configButton, SIGNAL (released()),this, SLOT (handleConfigButton()));
-    connect(configPath, SIGNAL (editingFinished()),this, SLOT (handleConfigEdit()));
-    QPushButton *clearConfigButton = new QPushButton("Clear", this);
-    connect(clearConfigButton, SIGNAL (released()),this, SLOT (handleClearConfigButton()));
-    configPath->setStyleSheet("border: 1px solid; padding: 10px");
-    layout->addWidget(note,2,0,1,-1);
-    layout->addWidget(configLabel,3,0);
-    layout->addWidget(configPath,3,1);
-    layout->addWidget(configButton,3,2);
-    layout->addWidget(clearConfigButton,3,3);
-
-#ifdef CONFIG_DIR_PATH
-    configPath->setEnabled(false);
-    configButton->setEnabled(false);
-    clearConfigButton->setEnabled(false);
-#endif
-
-    QString pluginPath = w->getSettings()->value("pluginDirPath").toString();
-    pluginPath.replace("$APP_PATH$", QCoreApplication::applicationDirPath());
-    QDir PluginDir(pluginPath);
-    QStringList Filter;
-    Filter.append("");
-    QStringList current;
-
+	
+	QLabel *inputLabel = new QLabel("Video Plugin", this);
+    layout->addWidget(inputLabel,0,0);
+    QComboBox *inputChoice = new QComboBox(this);
+    Filter.replace(0,"mupen64plus-video*");
+    current = PluginDir.entryList(Filter);
+    inputChoice->addItems(current);
+    QString qtVideoPlugin = w->getSettings()->value("gfxPlugin").toString();
+    int my_index = inputChoice->findText(qtVideoPlugin);
+    if (my_index == -1) {
+        inputChoice->addItem(qtVideoPlugin);
+        my_index = inputChoice->findText(qtVideoPlugin);
+    }
+    inputChoice->setCurrentIndex(my_index);
+    connect(inputChoice, static_cast<void(QComboBox::*)(const QString &)>(&QComboBox::activated),
+        [=](const QString &text) {
+            w->getSettings()->setValue("gfxPlugin", text);
+            w->updatePlugins();
+    });
+    layout->addWidget(inputChoice,0,1);
+	
+	QLabel *inputLabel = new QLabel("Audio Plugin", this);
+    layout->addWidget(inputLabel,1,0);
+    QComboBox *inputChoice = new QComboBox(this);
+    Filter.replace(0,"mupen64plus-audio*");
+    current = PluginDir.entryList(Filter);
+    inputChoice->addItems(current);
+    QString qtAudioPlugin = w->getSettings()->value("audioPlugin").toString();
+    int my_index = inputChoice->findText(qtAudioPlugin);
+    if (my_index == -1) {
+        inputChoice->addItem(qtAudioPlugin);
+        my_index = inputChoice->findText(qtAudioPlugin);
+    }
+    inputChoice->setCurrentIndex(my_index);
+    connect(inputChoice, static_cast<void(QComboBox::*)(const QString &)>(&QComboBox::activated),
+        [=](const QString &text) {
+            w->getSettings()->setValue("audioPlugin", text);
+            w->updatePlugins();
+    });
+    layout->addWidget(inputChoice,1,1);
+	
     QLabel *inputLabel = new QLabel("Input Plugin", this);
-    layout->addWidget(inputLabel,5,0);
+    layout->addWidget(inputLabel,2,0);
     QComboBox *inputChoice = new QComboBox(this);
     Filter.replace(0,"mupen64plus-input*");
     current = PluginDir.entryList(Filter);
@@ -151,7 +130,27 @@ void SettingsDialog::initStuff()
             w->getSettings()->setValue("inputPlugin", text);
             w->updatePlugins();
     });
-    layout->addWidget(inputChoice,5,1);
+    layout->addWidget(inputChoice,2,1);
+	
+	QLabel *inputLabel = new QLabel("RSP Plugin", this);
+    layout->addWidget(inputLabel,3,0);
+    QComboBox *inputChoice = new QComboBox(this);
+    Filter.replace(0,"mupen64plus-rsp*");
+    current = PluginDir.entryList(Filter);
+    inputChoice->addItems(current);
+    QString qtRSPPlugin = w->getSettings()->value("rspPlugin").toString();
+    int my_index = inputChoice->findText(qtRSPPlugin);
+    if (my_index == -1) {
+        inputChoice->addItem(qtRSPPlugin);
+        my_index = inputChoice->findText(qtRSPPlugin);
+    }
+    inputChoice->setCurrentIndex(my_index);
+    connect(inputChoice, static_cast<void(QComboBox::*)(const QString &)>(&QComboBox::activated),
+        [=](const QString &text) {
+            w->getSettings()->setValue("rspPlugin", text);
+            w->updatePlugins();
+    });
+    layout->addWidget(inputChoice,3,1);
 
     setLayout(layout);
 }
